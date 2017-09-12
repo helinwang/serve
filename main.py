185acd0bbbe55fbf24529371172489e399c8bad3
Yu Yang <yuyang18@baidu.com>
2017-09-11 18:23:07 -0700

    Complete Inference Service

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-import numpy as np
-from flask import Flask, jsonify, request
+import os
 import traceback
+
 import paddle.v2 as paddle
+from flask import Flask, jsonify, request
+
+tarfn = os.getenv('PARAMETER_TAR_PATH', None)
+
+if tarfn is None:
+    raise ValueError("parameter tar file is not specific")
+
+topology_filepath = os.getenv('TOPOLOGY_FILE_PATH', None)
+
+if topology_filepath is None:
+    raise ValueError("topology file is not specific")
+
+port = int(os.getenv('PORT', '80'))
 
 app = Flask(__name__)
 
@@ -9,35 +22,31 @@ app = Flask(__name__)
 def errorResp(msg):
     return jsonify(code=-1, message=msg)
 
+
 def successResp(data):
     return jsonify(code=0, message="success", data=data)
 
-@app.route('/', methods=['POST'])
-def mnist():
-    global inferer
-    feeding = {}
-    d = []
-    for i, key in enumerate(request.json):
-        d.append(request.json[key])
-        feeding[key] = i
 
+@app.route('/', methods=['POST'])
+def infer():
+    global inferer
     try:
+        feeding = {}
+        d = []
+        for i, key in enumerate(request.json):
+            d.append(request.json[key])
+            feeding[key] = i
         r = inferer.infer([d], feeding=feeding)
-    except Exception as e:
+    except:
         trace = traceback.format_exc()
-        print trace
         return errorResp(trace)
     return successResp(r.tolist())
 
+
 if __name__ == '__main__':
-    global inferer
     paddle.init()
-    images = paddle.layer.data(
-        name='pixel', type=paddle.data_type.dense_vector(784))
-    label = paddle.layer.data(
-        name='label', type=paddle.data_type.integer_value(10))
-    predict = paddle.layer.fc(input=images, size=10, act=paddle.activation.Softmax())
-    cost = paddle.layer.classification_cost(input=predict, label=label)
-    parameters = paddle.parameters.create(cost)
-    inferer = paddle.inference.Inference(output_layer=predict, parameters=parameters)
-    app.run(host='0.0.0.0', port=80)
+    with open(tarfn) as param_f, open(topology_filepath) as topo_f:
+        params = paddle.parameters.Parameters.from_tar(param_f)
+        inferer = paddle.inference.Inference(parameters=params, fileobj=topo_f)
+    print 'serving on port ', port
+    app.run(host='0.0.0.0', port=port)
